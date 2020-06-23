83b48bf020c169adbe40b85c3c508df426a412ce
nickgkd <nikhil.gaikwad@optanix.com>
2020-06-23 19:18:11 +0530

    2. Decryption Logic

--- Encryption/src/main/java/com/incubyte/calculator/TestEnryDecry.java
+++ Encryption/src/main/java/com/incubyte/calculator/TestEnryDecry.java
@@ -2,33 +2,63 @@ package com.incubyte.calculator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class TestEnryDecry {
-	
+
 	public String encrypt(String str) {
 		String alpa = "abcdefghijklmnopqrstuvwxyz";
 		String key = "!)\"(£*%&><@abcdefghijklmno";
-		
+
 		char[] alpaArr = alpa.toCharArray();
 		char[] keyArr = key.toCharArray();
-		
-		Map<Character,Character> hs = new HashMap<>();
-		for(int i=0;i<alpaArr.length;i++) {
-				hs.put(alpaArr[i],keyArr[i]);
+
+		Map<Character, Character> hs = new HashMap<>();
+		for (int i = 0; i < alpaArr.length; i++) {
+			hs.put(alpaArr[i], keyArr[i]);
 		}
-		
+
 		StringBuffer sb = new StringBuffer();
 		char[] strArr = str.toCharArray();
-		for(int i=0;i<strArr.length;i++) {
-			if(hs.containsKey(strArr[i])) {
-				 Character value= hs.get(strArr[i]);
-				 sb.append(value);
+		for (int i = 0; i < strArr.length; i++) {
+			if (hs.containsKey(strArr[i])) {
+				Character value = hs.get(strArr[i]);
+				sb.append(value);
 			}
 		}
-			
+
 		String str1 = sb.toString();
-		System.out.println("val->"+ str1);
+		System.out.println("val->" + str1);
 		return str1;
 	}
 
+	public String decrypt(String str) {
+		String alpa = "abcdefghijklmnopqrstuvwxyz";
+		String key = "!)\"(£*%&><@abcdefghijklmno";
+
+		char[] alpaArr = alpa.toCharArray();
+		char[] keyArr = key.toCharArray();
+
+		Map<Character, Character> hs = new HashMap<>();
+		for (int i = 0; i < alpaArr.length; i++) {
+			hs.put(alpaArr[i], keyArr[i]);
+		}
+
+		StringBuffer sb = new StringBuffer();
+		char[] strArr = str.toCharArray();
+		for (int i = 0; i < strArr.length; i++) {
+			for (Entry<Character, Character> entry : hs.entrySet()) {
+				if (entry.getValue().equals(strArr[i])) {
+					Character value = entry.getKey();
+					sb.append(value);
+				}
+			}
+
+		}
+
+		String str1 = sb.toString();
+		System.out.println("val->" + str1);
+		return str1;
+
+	}
 }
--- Encryption/src/test/java/com/incubyte/calculator/Demo.java
+++ Encryption/src/test/java/com/incubyte/calculator/Demo.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
+
 public class Demo extends EncryptionApplicationTests{
 	
 	
@@ -16,7 +17,19 @@ public class Demo extends EncryptionApplicationTests{
 	@Test
 	public void testTwo() {
 		TestEnryDecry test = new TestEnryDecry();
-		assertEquals(test.encrypt("Manchester"),"h!b");
+		assertEquals(test.encrypt("Manchester"),"!c\"&£hi£g");
+	}
+	
+	@Test
+	public void testThree() {
+		TestEnryDecry test = new TestEnryDecry();
+		assertEquals(test.decrypt("h!b"),"sam");
+	}
+	
+	@Test
+	public void testFour() {
+		TestEnryDecry test = new TestEnryDecry();
+		assertEquals(test.decrypt("Manchester"),"!c\"&£hi£g");
 	}
 
 }
